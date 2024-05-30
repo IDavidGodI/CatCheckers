c4e0bedf6e77053401dfcdf5c0eff49679ab76b3
IDavidGodI <c6099020@gmail.com>
2024-05-30 02:49:49 -0500

    Prueba basica

---
+++ src/main.py
@@ -0,0 +1,125 @@
+from paths import Paths
+import pygame as pg
+import json
+import os
+
+
+WINDOW_SIZE = (1280,720)
+window = pg.display.set_mode(WINDOW_SIZE)
+
+pg.display.set_icon(pg.image.load("./src/assets/icono.ico"))
+
+running = True
+
+class SetUP:
+  data: dict
+  def __init__(self, paths: Paths):
+    self.loadData(paths)
+    
+  def loadData(self, paths: Paths):
+    mainSetupFilePath = paths.getMainSetupPath()
+    themeSetupFilePath = paths.getThemeSetupPath()
+    with open(mainSetupFilePath) as mainSetupFile:
+      self.data = json.load(mainSetupFile)
+      with open(themeSetupFilePath) as themeSetupFile:
+        self.data |= json.load(themeSetupFile)
+
+
+class AbstractSprite:
+  sprite: pg.Surface
+  def __init__(self, sprite: pg.Surface = None, dimensions = (0,0), index: int = 0):
+    if (sprite):
+      self.setSprite(sprite, dimensions, index)
+
+  def setSprite(self, sprite: pg.Surface, dimensions: tuple, index: int):
+    
+    self.sprite = pg.Surface(dimensions)
+    
+    s = (index*dimensions[0])
+    self.sprite.blit(sprite,(0,0), (s,0)+dimensions)
+  
+  def render(self, surface: pg.Surface, coordinate: tuple):
+    surface.blit(self.sprite, coordinate)
+
+
+
+
+class Theme:
+  setup: SetUP
+  lightSprites: list[AbstractSprite]
+  darkSprites: list[AbstractSprite]
+  board: pg.Surface
+  colorPalette: dict
+
+  def __init__(self, themeName: str):
+    self.paths = Paths(themeName)
+    self.setup = SetUP(self.paths)
+    self.lightSprites = []
+    self.darkSprites = []
+    self.loadTheme()
+
+  def loadTheme(self):
+    piecesSetUP = self.setup.data["pieces"]
+    boardSetUP = self.setup.data["board"]
+    self.colorPalette = self.setup.data["colorPalette"]
+    
+    self.board = pg.image.load(self.paths.getBoardPath())
+    dimensions = (piecesSetUP.get("dimensions"),)*2
+    for animacion in piecesSetUP["flow"]:
+      name = animacion.get("name")
+      darkSprites = pg.image.load(self.paths.getDarkSpritesPath(name))
+      lightSprites = pg.image.load(self.paths.getLightSpritesPath(name))
+      static = animacion.get("static")
+      if (static):
+        for index in static:
+          self.lightSprites.append(AbstractSprite(lightSprites, dimensions, index-1))
+          self.darkSprites.append(AbstractSprite(darkSprites, dimensions, index-1))
+        
+
+
+      
+    
+
+    
+
+    
+
+  
+
+
+theme = Theme("Placeholder")
+# gameSurface = pg.Surface(())
+
+clock = pg.time.Clock()
+window.fill(theme.colorPalette["primary"])
+
+
+piecesize = theme.setup.data.get("pieces").get("dimensions")
+boardoffset = theme.setup.data.get("board").get("offset")
+
+while running:
+  clock.tick(12)
+  events = pg.event.get()
+  
+  for event in events:
+    if (event.type == pg.QUIT): 
+      running=False
+      exit()
+
+  for x in range(0,8):
+    for y in range(0,3):
+      for piece in theme.lightSprites:
+        if (x%2 ^ y%2):
+          piece.render(theme.board,(x*piecesize + boardoffset, y*piecesize + boardoffset))
+
+  for x in range(0,8):
+    for y in range(5,8):
+      for piece in theme.darkSprites:
+        if (x%2 ^ y%2):
+          piece.render(theme.board,(x*piecesize + boardoffset, y*piecesize + boardoffset))
+  
+  
+  scaled_board=pg.transform.scale(theme.board, (WINDOW_SIZE[1],)*2)
+
+  window.blit(scaled_board,(0,0))
+  pg.display.update()
---
+++ src/paths.py
@@ -0,0 +1,44 @@
+from os import path
+
+ROOT_PATH = "src"
+ASSETS_PATH = path.join(ROOT_PATH,"assets")
+THEMES_PATH = path.join(ASSETS_PATH,"themes")
+
+
+
+DARK_PIECES_FILE = "dark_%s.png"
+LIGHT_PIECES_FILE = "light_%s.png"
+BOARD_FILE = "board.png"
+SETUP_FILE = "setup.json"
+
+
+class Paths:
+  themePath: str
+
+  def __init__(self, themeName = None):
+    if (themeName):
+      self.themePath = self.getThemePath(themeName)
+
+  def joinThemeAsset(self, assetName: str):
+    directory = path.join(self.themePath, assetName)
+    if path.isfile(directory):
+      return directory
+  def getDarkSpritesPath(self, animacion: str) -> str:
+    return self.joinThemeAsset(DARK_PIECES_FILE%animacion)
+  def getLightSpritesPath(self,animacion: str) -> str:
+    return self.joinThemeAsset(LIGHT_PIECES_FILE%animacion)
+  def getBoardPath(self) -> str:
+    return self.joinThemeAsset(BOARD_FILE)
+  def getThemeSetupPath(self) -> str:
+    return self.joinThemeAsset(SETUP_FILE)
+
+  def getMainSetupPath(self) -> str:
+    directory = path.join(THEMES_PATH, SETUP_FILE)
+    if path.isfile(directory):
+      return directory
+
+  def getThemePath(self,themeName: str) -> str | None :
+    directory = path.join(THEMES_PATH, themeName)
+    if path.isdir(directory):
+      return directory
+  
